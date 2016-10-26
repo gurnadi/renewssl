9f0fa8b2af7227feca58fab4eb1012f81dcff300
Dudi Gurnadi K <gurnadi@gmail.com>
2016-10-26 10:49:43 +0700

    quiet process letsencrypt

--- renewssl.sh
+++ renewssl.sh
@@ -10,16 +10,57 @@ for EACHDOMAIN in $DOMAIN; do
   DATEEXP=`echo $EXPIRE | awk -F" " '{print $2}'`;
   YEAREXP=`echo $EXPIRE | awk -F" " '{print $4}'`;
   MONTHEXP=`echo $EXPIRE | awk -F" " '{print $1}' | awk -F"=" '{print $2}'`;
+  
+  case $MONTHEXP in
+        Jan)
+          ANGKAMON="01";
+          ;;
+        Feb)
+          ANGKAMON="02";
+          ;;
+        Mar)
+          ANGKAMON="03";
+          ;;
+        Apr)
+          ANGKAMON="04"
+          ;;
+        May)
+          ANGKAMON="05";
+          ;;
+        Jun)
+          ANGKAMON="06";
+          ;;
+        Jul)
+          ANGKAMON="07";
+          ;;
+        Aug)
+          ANGKAMON="08";
+          ;;
+        Sep)
+          ANGKAMON="09";
+          ;;
+        Oct)
+          ANGKAMON="10";
+          ;;
+        Nov)
+          ANGKAMON="11";
+          ;;
+        Dec)
+          ANGKAMON="12";
+          ;;
+  esac
 
   if [ "$DATEEXP" = "1" ]; then DATEEXP=29; fi
   DATEEXEC=`echo $(($DATEEXP-1))`;
   if [ $DATEEXEC -lt 10 ]; then DATEEXEC="0$DATEEXEC"; fi;
   DATENOW=`date "+%b %d %Y"`;
+  DATENOW2=`date +%Y%m%d`;
+  DATETEK="$YEAREXP$ANGKAMON$DATEEXP";
 
-  if [ "$DATENOW" = "$MONTHEXP $DATEEXEC $YEAREXP" ]; then
-        $LETSBIN --agree-tos --config $CONFIGSSL/$EACHDOMAIN.ini --debug certonly
+  if [ "$DATENOW" = "$MONTHEXP $DATEEXEC $YEAREXP" ] || [ $DATENOW2 -gt $DATETEK ]; then
+        $LETSBIN -q --agree-tos --config $CONFIGSSL/$EACHDOMAIN.ini certonly
         # if you are running python27 from scl repository on CentOS 6.X uncomment this command below and comment out the above command
-        ##scl enable python27 "$LETSBIN --agree-tos --config $CONFIGSSL/$EACHDOMAIN.ini --debug certonly"
+        ##scl enable python27 "$LETSBIN -q --agree-tos --config $CONFIGSSL/$EACHDOMAIN.ini certonly"
         echo "Update SSL Certificate $EACHDOMAIN on $DATENOW" >> $LOGSSLFILE;
         /etc/init.d/httpd restart
   fi
